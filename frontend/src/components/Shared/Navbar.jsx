f0c5e68f36dbfd18e41527f588f80d7f1708f4a2
gourav_kumar <gourav12303012@gmail.com>
2025-04-05 09:06:01 +0530

    firstcommit

--- frontend/src/components/Shared/Navbar.jsx
+++ frontend/src/components/Shared/Navbar.jsx
@@ -28,6 +28,7 @@ function Navbar() {
             <div className=' justify-center items-center gap-6 text-xl hidden md:flex'>
                 <NavLink style={navLinkStyle} to="/">Home</NavLink>
                 <NavLink style={navLinkStyle} to="/appointment">Appointment</NavLink>
+                <NavLink style={navLinkStyle} to="/services">Services</NavLink>
                 <NavLink style={navLinkStyle} to="/about-us">About Us</NavLink>
                 <NavLink style={navLinkStyle} to="/contact-us">Contact Us</NavLink>
                 <button className='bg-slate-900 text-white p-1 pe-2 ps-2 rounded-full hover:scale-110 hover:bg-slate-800 duration-300 active:scale-90' onClick={handleClick}>LogIn</button>
@@ -36,6 +37,7 @@ function Navbar() {
             <div className={!isMobNav?'hidden':'flex flex-col absolute top-0 left-0 h-screen w-screen text-white text-xl justify-center items-center bg-black md:hidden '}>
                 <NavLink className="py-6 text-3xl"  style={navLinkStyle} to="/">Home</NavLink>
                 <NavLink className="py-6 text-3xl" style={navLinkStyle} to="/appointment">Appointment</NavLink>
+                <NavLink className="py-6 text-3xl" style={navLinkStyle} to="/services">Services</NavLink>
                 <NavLink className="py-6 text-3xl"  style={navLinkStyle} to="/about-us">About Us</NavLink>
                 <NavLink className="py-6 text-3xl"  style={navLinkStyle} to="/contact-us">Contact Us</NavLink>
                 <NavLink className="py-6 text-3xl"  style={navLinkStyle} to="/sign-in">SignIn</NavLink>
